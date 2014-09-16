48c0e72d7df002c3e6548b8453a0781e7efacbdc
Kristian Glass <git@doismellburning.co.uk>
2014-09-16 08:52:46 +0100

    Revert to original try/except style, this time with correct Exception!

--- djtranslationchecker/management/commands/translationchecker.py
+++ djtranslationchecker/management/commands/translationchecker.py
@@ -55,7 +55,10 @@ def check_po(command, po_filepath):
 
 
 def get_po_filepaths():
-    apps = getattr(settings, 'PROJECT_APPS', settings.INSTALLED_APPS)
+    try:
+        apps = settings.PROJECT_APPS
+    except AttributeError:
+        apps = settings.INSTALLED_APPS
 
     pos = []
     for app_name in apps:
